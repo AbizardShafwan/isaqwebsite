09d1b7db383368977e6dadc5489a64b5be4eed18
abizardshafwan <abizardshafwan@gmail.com>
2020-08-16 10:44:43 +1000

    Added certbot manually

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,9 @@
+echo chmod init-letsencrypt.sh
+chmod +x init-letsencrypt.sh
+
+echo running ini-letsencrypt.sh
+sudo ./init-letsencrypt.sh
+
 echo killing old docker processes
 docker-compose rm -fs
 
