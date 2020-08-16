ba7e6456b73ccb70ddecb32b1f713dbe4a27e9fd
abizardshafwan <abizardshafwan@gmail.com>
2020-08-16 13:24:14 +1000

    Update letsencrypt and bash run

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -22,18 +22,18 @@ fi
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
   mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
   echo
 fi
 
 echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
+cert_path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
 docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
+    -keyout '$cert_path/privkey.pem' \
+    -out '$cert_path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
 echo
 
--- run_docker.sh
+++ run_docker.sh
@@ -2,8 +2,5 @@ chmod +x init-letsencrypt.sh
 
 sudo ./init-letsencrypt.sh
 
-echo killing old docker processes
-docker-compose rm -fs
-
 echo building docker containers
-docker-compose up --build -d
+docker-compose up
