7addbc292c0399d480110674a758680c95f9bf64
abizardshafwan <abizardshafwan@gmail.com>
2020-08-16 10:54:02 +1000

    Update

--- run_docker.sh
+++ run_docker.sh
@@ -2,7 +2,7 @@ echo chmod init-letsencrypt.sh
 chmod +x init-letsencrypt.sh
 
 echo running ini-letsencrypt.sh
-sudo ./init-letsencrypt.sh
+./init-letsencrypt.sh
 
 echo killing old docker processes
 docker-compose rm -fs
